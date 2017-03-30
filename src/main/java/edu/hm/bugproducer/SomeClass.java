bf9336caa07961d60c8d2eb6bec628d7f22b967d
Tom Maier <maier14@hm.edu>
2017-03-30 11:39:57 +0200

    Add some classes

--- src/main/java/edu/hm/bugproducer/SomeClass.java
+++ src/main/java/edu/hm/bugproducer/SomeClass.java
@@ -5,8 +5,8 @@ import java.util.Date;
 public class SomeClass {
     @RenderMe
     private int foo;
-    @RenderMe(with = "edu.hm.renderer.ArrayRenderer")
-    int[] array = {1, 2, 3,};
+    /*@RenderMe(with = "edu.hm.renderer.ArrayRenderer")
+    int[] array = {1, 2, 3,};*/
     @RenderMe
     private Date date = new Date(123456789);
 
