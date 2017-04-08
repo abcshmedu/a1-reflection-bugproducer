c04cdb5c581acf163026aec9368bc8db9f2b77dc
Tom Maier <maier14@hm.edu>
2017-04-08 10:55:39 +0200

    add Parameterized

--- src/main/java/edu/hm/bugproducer/SomeClass.java
+++ src/main/java/edu/hm/bugproducer/SomeClass.java
@@ -6,12 +6,28 @@ public class SomeClass {
     @RenderMe
     private int foo;
     @RenderMe(with = "edu.hm.bugproducer.ArrayRenderer")
-    int[] array = {1, 2, 3};
+    int[] array;
     @RenderMe
-    private Date date = new Date(123456789);
+    private Date date;
 
 
     public SomeClass(int foo) {
         this.foo = foo;
+    }
+
+    public SomeClass() {
+        foo = 3;
+        array = new int[]{1, 2, 3};
+        date = new Date(123456789);
+    }
+
+    public SomeClass(int[] array) {
+        this.array = array;
+    }
+
+    public SomeClass(int foo, int[] array, Date date) {
+        this.foo = foo;
+        this.array = array;
+        this.date = date;
     }
 }
--- src/test/java/edu/hm/bugproducer/RendererTest.java
+++ src/test/java/edu/hm/bugproducer/RendererTest.java
@@ -2,60 +2,141 @@ package edu.hm.bugproducer;
 
 
 import org.junit.*;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
+@RunWith(Enclosed.class)
 public class RendererTest {
-    private SomeClass toRender;
-    private Renderer renderer;
 
-    private int foo;
-    private int[] intArray;
-    private Date date;
+    public static class DefaultConstructor {
 
-    public RendererTest(int foo, int[] intArray, Date date) {
-        this.foo = foo;
-        this.intArray = intArray;
-        this.date = date;
+        private SomeClass toRender;
+        private Renderer renderer;
+
+        @Test
+        public void testRenderingDefault() throws Exception {
+            toRender = new SomeClass();
+            renderer = new Renderer(toRender);
+
+            assertEquals("Instance of edu.hm.bugproducer.SomeClass\n" +
+                            "foo (Type int): 3\narray (Type int[]) [1, 2, 3, ]\n" +
+                            "date (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n",
+                    renderer.render());
+        }
     }
 
-    @Parameterized.Parameters
-    public static Collection primeNumbers() {
-        return Arrays.asList(new Object[][]{
-                {2, new int[]{1, 2, 3}, new Date(123456789)},
-                {5, new int[]{1, 2, 3}, new Date(123456788)}
-        });
+    @RunWith(Parameterized.class)
+    public static class FirstParameterizedPart {
+
+        private SomeClass toRender;
+        private Renderer renderer;
+
+        private int foo;
+        private int[] intArray;
+        private Date date;
+
+        public FirstParameterizedPart(int foo, int[] intArray, Date date) {
+            this.foo = foo;
+            this.intArray = intArray;
+            this.date = date;
+        }
+
+        @Parameterized.Parameters
+        public static Collection myArray() {
+            return Arrays.asList(new Object[][]{
+                    {2, new int[]{1, 2, 3}, new Date(123456789)},
+                    {Integer.MAX_VALUE, new int[]{1, 2, 3}, new Date(123456789)},
+                    {Integer.MIN_VALUE, new int[]{1, 2, 3}, new Date(123456789)}
+            });
+        }
+
+        @Test
+        public void testRenderingAllParameter() throws Exception {
+            toRender = new SomeClass(foo, intArray, date);
+            renderer = new Renderer(toRender);
+
+            assertEquals("Instance of edu.hm.bugproducer.SomeClass\n" +
+                            "foo (Type int): " + foo + "\narray (Type int[]) [" + intArray[0] + ", " + intArray[1] + ", " + intArray[2] + ", ]\n" +
+                            "date (Type java.util.Date): " + date + "\n",
+                    renderer.render());
+        }
+
     }
 
-    @Test
-    public void testRendering() throws Exception {
-        toRender = new SomeClass(foo);
-        renderer = new Renderer(toRender);
+    @RunWith(Parameterized.class)
+    public static class FooConstructorParameterized {
 
-        assertEquals("Instance of edu.hm.bugproducer.SomeClass\n" +
-                        "foo (Type int): " + foo + "\narray (Type int[]) ["+intArray[0]+", " + intArray[1]+", " +intArray[2]+", ]\n" +
-                        "date (Type java.util.Date): "+date+"\n",
-                renderer.render());
+
+        private SomeClass toRender;
+        private Renderer renderer;
+
+        private int foo;
+
+        public FooConstructorParameterized(int foo) {
+            this.foo = foo;
+        }
+
+        @Parameterized.Parameters
+        public static Collection myArray() {
+            return Arrays.asList(new Object[][]{
+                    {2}, {Integer.MIN_VALUE}, {Integer.MAX_VALUE}});
+        }
+
+
+        @Test(expected = NullPointerException.class)
+        public void testArrayNullPointer() throws InvocationTargetException, NoSuchMethodException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+            toRender = new SomeClass(foo);
+
+            assertEquals("Instance of edu.hm.bugproducer.SomeClass\n" +
+                            "foo (Type int): " + foo + "\narray (Type int[]) []\n" +
+                            "date (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n",
+                    new Renderer(toRender).render());
+        }
+
     }
 
-    @Test(expected = NullPointerException.class)
-    public void testNullPointerRendering() throws Exception {
-        //toRender = new SomeClass(5);
-        renderer = new Renderer(toRender);
-        renderer.render();
+    @RunWith(Parameterized.class)
+    public static class ArrayParameterizedPart {
+
+
+        private SomeClass toRender;
+        private Renderer renderer;
+
+        private int[] intArray;
+
+        public ArrayParameterizedPart(int[] intArray) {
+            this.intArray = intArray;
+        }
+
+        @Parameterized.Parameters
+        public static Collection myArray() {
+            return Arrays.asList(new Object[][]{
+                    {null}});
+        }
+
+
+        @Test(expected = NullPointerException.class)
+        public void testArrayNullPointer() throws InvocationTargetException, NoSuchMethodException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+            toRender = new SomeClass(intArray);
+            new Renderer(toRender).render();
+        }
+
+        @Test(expected = NullPointerException.class)
+        public void testNullPointerRendering() throws Exception {
+            renderer = new Renderer(toRender);
+            renderer.render();
+        }
     }
 
-    @Test(expected = NullPointerException.class)
-    public void testArray() throws Exception {
-        //toRender = new SomeClass(5);
-        renderer = new Renderer(toRender);
-        renderer.render();
-    }
+
 }
